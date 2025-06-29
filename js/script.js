699b6dff41d3206cc163b65996566943cae7314d
Neha Dahiya <nehadahiya7082003@gmail.com>
2025-06-29 17:03:23 +0530

    media-query

--- js/script.js
+++ js/script.js
@@ -27,9 +27,37 @@ window.onscroll = () => {
       });
     }
   });
-
   // sticky navbar
   let header = document.querySelector("header");
 
   header.classList.toggle("sticky", window.scrollY > 100);
+
+  //   remove toggle icon and navbar when navbar link is clicked(scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
+
+// scroll reveal
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+// typed js
+const typed = new Typed(".multiple-text", {
+  strings: ["Data Analyst", "SEO Content Writer", "Blogger"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
